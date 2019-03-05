a2a4d730eed0719f944b82d91c252da67ea01f7a
Vladimir_Yaroslavtsev <yaroslavtsev1395@gmial.com>
2019-03-05 00:37:19 -0800

    Use syntax @decorator. Converted func(address) to str. Fixed "create table" - add type(text) to column. Rename: - cursor > cur; - column "text" > attribute_text; - site > line. Delete result = []. Add to .gitignore database file.

--- func_decor.py
+++ func_decor.py
@@ -8,19 +8,11 @@ import random
 from lxml import html
 
 
-def get_random_text(address):
-    """
-    Get random text in all HTML page.
-    :param address: some url.
-    :return: randomly selected text
-    from the entire text.
-    """
-
-    page_html = requests.get(address).content.decode('utf-8')
-    tree = html.fromstring(page_html)
-    all_text = tree.xpath('./body//*/text()')
-    rand_txt = random.choice(all_text)
-    return rand_txt
+url = 'https://www.google.com'
+conn = sqlite3.connect("site_checked.db")
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS randtext (url text, attribute_text text)")
+conn.commit()
 
 
 def write_data_to_database(func):
@@ -32,7 +24,7 @@ def write_data_to_database(func):
     """
 
     def decorator1(address):
-        cursor.execute("INSERT INTO randtext VALUES (?, ?)", (address, func(address)))
+        cur.execute("INSERT INTO randtext VALUES (?, ?)", (address, str(func(address))))
         conn.commit()
     return decorator1
 
@@ -42,21 +34,20 @@ def check_url_in_database(func):
     Decorator, who checked url in database.
     If the url is in the database, it return
     all the 'text' lines from the table, otherwise
-    th text is taken from the get_random_text(address)
-    :param func: get_random_text(address)
+    the text is taken from the get_random_text(address).
+    :param func: get_random_text.
     :return: text or list with 'text' lines.
     """
 
     def decorator2(address):
         sql = "SELECT * FROM randtext WHERE url=?"
-        cursor.execute(sql, [url])
-        info_lsit = cursor.fetchall()
-        result = []
-        for site in info_lsit:
-            if address in site:
-                sql = "SELECT text FROM randtext WHERE url=?"
-                cursor.execute(sql, [url])
-                txt_list = cursor.fetchall()
+        cur.execute(sql, [url])
+        info_list = cur.fetchall()
+        for line in info_list:
+            if address in line:
+                sql = "SELECT attribute_text FROM randtext WHERE url=?"
+                cur.execute(sql, [url])
+                txt_list = cur.fetchall()
                 result = [txt_list[i][0] for i in range(len(txt_list))]
                 return result
             else:
@@ -64,21 +55,28 @@ def check_url_in_database(func):
     return decorator2
 
 
+@write_data_to_database
+@check_url_in_database
+def get_random_text(address):
+    """
+    Get random text in all HTML page.
+    :param address: some url.
+    :return: randomly selected text
+    from the entire text.
+    """
+
+    page_html = requests.get(address).content.decode('utf-8')
+    tree = html.fromstring(page_html)
+    all_text = tree.xpath('./body//*/@id')
+    rand_txt = random.choice(all_text)
+    return rand_txt
+
+
 def main():
-    print(get_random_text(url))
-    write_data = write_data_to_database(get_random_text)
-    write_data(url)
-    check_data = check_url_in_database(get_random_text)
-    print(check_data(url))
+    get_random_text(url)
 
 
 if __name__ == '__main__':
-    url = 'https://www.yandex.ru'
-    conn = sqlite3.connect("site_checked.db")
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE  IF NOT EXISTS randtext ('url', 'text')""")
-    conn.commit()
-
     main()
     conn.commit()
     conn.close()
