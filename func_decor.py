70f5969b9498a380a9f950e29c022d30e51cd0b2
Vladimir_Yaroslavtsev <yaroslavtsev1395@gmial.com>
2019-03-05 22:29:35 -0800

    Variant with two decorators, who works normally. For each site in table write one text from this site. @cached goes first and checked table for site availability.

--- func_decor.py
+++ func_decor.py
@@ -8,48 +8,52 @@ import random
 from lxml import html
 from functools import wraps
 
-
-url = "https://www.yandex.ru"
+url = "https://www.github.com"
 conn = sqlite3.connect("site_checked.db")
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS randtext(url text, attribute_text text)")
 conn.commit()
 
 
-def cached(func):
+def write_data_to_database(func):
     """
-    Decorator, who write result of get_random_text(address)
-     in database and checked url in database.
-    If the url is in the database, it return
-    all the 'text' lines from the table, otherwise
-    the text is taken from the get_random_text(address).
+    Decorator, who write result of
+    get_random_text(address) in database.
     :param func: get_random_text.
-    :return: text or list with 'text' lines.
+    :return: wrapper
     """
+
     @wraps(func)
     def wrapper(address):
+        return cur.execute("INSERT INTO randtext VALUES (?, ?)", (address, str(func(address))))
+    return wrapper
 
+
+def cached(func):
+    """
+    Decorator, who checked url in database.
+    If the url is in the database, it return
+    text line from the table, otherwise
+    the text is taken from the get_random_text(address).
+    :param func: get_random_text.
+    :return: text from selected site.
+    """
+
+    @wraps(func)
+    def wrapper(address):
         sql = "SELECT * FROM randtext WHERE url=?"
         cur.execute(sql, [url])
         info_list = cur.fetchall()
         if not info_list:
-            func(address)
-            return cur.execute("INSERT INTO randtext VALUES (?, ?)", (address, str(func(address))))
+            return func(address)
         else:
-            cur.execute("INSERT INTO randtext VALUES (?, ?)", (address, str(func(address))))
-            for line in info_list:
-                if address in line:
-                    sql = "SELECT attribute_text FROM randtext WHERE url=?"
-                    cur.execute(sql, [url])
-                    txt_list = cur.fetchall()
-                    result = [txt_list[i][0] for i in range(len(txt_list))]
-                    return result
-                else:
-                    return func(address)
+            result = info_list[0][1]
+            return result
     return wrapper
 
 
 @cached
+@write_data_to_database
 def get_random_text(address):
     """
     Get random text in all HTML page.
@@ -62,6 +66,7 @@ def get_random_text(address):
     tree = html.fromstring(page_html)
     all_text = tree.xpath('./body//*/@id')
     rand_txt = random.choice(all_text)
+    print(rand_txt)
     return rand_txt
 
 
@@ -70,7 +75,6 @@ def main():
 
 
 if __name__ == '__main__':
-
     main()
     conn.commit()
     conn.close()
